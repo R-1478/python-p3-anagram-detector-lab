feb8bc7716b91d982f507662321ca32d974780f4
evans <evansnt9619@gmail.com>
2023-12-11 21:49:56 +0300

    Completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.anagram = word
+
+    def match(self, word_list):
+        anagram_matches = [name for name in word_list if sorted(self.anagram) == sorted(name)]
+        return anagram_matches
